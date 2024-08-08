30c8ad07f5a913b28fc259196cdc2dba13663e24
PosAlex73 <alternativa988@inbox.ru>
2024-08-08 10:19:31 +0400

    Второе задание

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
-def sum_negative_between_min_max(arr: list):
-    """Функция для подсчета суммы между минимальным
-    и максимальным отрицательным значением элементов
-     массива"""
+class BaseClass:
+    def __init__(self, base_value):
+        self.base_value = base_value
 
-    # Если длина массива меньше 2, то нет смысла рассчитывать сумму.
-    if len(arr) < 2:
-        return 0
+    def display_base(self):
+        print(f"Base Value: {self.base_value}")
 
-    # Вычисляем ключи для минимального и максимального значения массива.
-    min_value = min(arr)
-    max_value = max(arr)
-    min_index = arr.index(min_value)
-    max_index = arr.index(max_value)
+    def base_method(self):
+        print("This is a method from BaseClass")
 
-    # Если максимальное значение равно минимального, то нет смысла высчитывать сумму.
-    if min_index == max_index:
-        return 0
 
-    # Если индекс минимального числа расположен после максимального, то меняем их местами.
-    if min_index > max_index:
-        min_index, max_index = max_index, min_index
+class DerivedClass(BaseClass):
+    def __init__(self, base_value, derived_value):
+        super().__init__(base_value)
+        self.derived_value = derived_value
 
-    # Агрегация суммы отрицательных элементов
-    sum_negative = 0
-    for i in range(min_index + 1, max_index):
-        if arr[i] < 0:
-            sum_negative += arr[i]
+    def display_derived(self):
+        print(f"Derived Value: {self.derived_value}")
 
-    return sum_negative
+    def derived_method(self):
+        print("This is a method from DerivedClass")
 
-# Пример использования
-arr = [3, -2, 1, -4, -1, -2, 5, -1, 2]
-print(sum_negative_between_min_max(arr))
+
+def test_program():
+    # Создаем экземпляр производного класса
+    derived_instance = DerivedClass(base_value=10, derived_value=20)
+
+    # Вызываем методы базового класса
+    derived_instance.display_base()
+    derived_instance.base_method()
+
+    # Вызываем методы производного класса
+    derived_instance.display_derived()
+    derived_instance.derived_method()
+
+
+if __name__ == "__main__":
+    test_program()
